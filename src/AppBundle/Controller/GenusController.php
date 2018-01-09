625144538262e45f3d12a406fa6139af654a1fda
anne.hilario <anne.hilario@ccf.org.ph>
2018-01-09 17:37:31 +0800

    Learning more on basics of basics

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -19,10 +19,16 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
-        $templating = $this->container->get('templating'); 
+        $notes = [
+            'Octopus asked me a riddle, outsmarted me',
+            'I counted 8 legs...as they wrapped around me',
+            'Inked!'  
+        ];     
+        
         return $this->render('genus/show.html.twig', [
-            'name' => $genusName
+            'name' => $genusName,  
+            'notes' => $notes
         ]);
 
     }
-}
+}  
